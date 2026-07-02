074f5da133580456c3edffc03bd384daea98cb2a
flytachi <jasur.rakhmatov03@gmail.com>
2026-07-02 14:33:47 +0500

    feat: add LaunchSpec DTO

---
+++ src/LaunchSpec.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+namespace Flytachi\Winter\Thread;
+
+final readonly class LaunchSpec
+{
+    public function __construct(
+        public string $payload,
+        public string $namespace = '',
+        public string $name = 'anonymous',
+        public ?string $tag = null,
+        public array $arguments = [],
+        public bool $debug = false,
+        public ?string $output = '/dev/null',
+        public bool $detached = false,
+    ) {}
+}
